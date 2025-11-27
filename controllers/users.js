60dac8678ca4fbca27632eaf1ef6e7656146bf87
Arthur BARBOSA <arthurbarbo177@gmail.com>
2025-11-27 20:17:11 +0000

    implementação e funcionamento no postman das routes /register e /login

---
+++ controllers/users.js
@@ -0,0 +1,80 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { registerSchema, loginSchema } from "../middlewares/userValidator.js";
+
+export const registerUser = async (req, res) => {
+  try {
+    const { error } = registerSchema.validate(req.body);
+    if (error)
+      return res.status(400).json({ message: error.details[0].message });
+
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Todos os campos são obrigatórios" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email já cadastrado" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      message: "Usuário criado com sucesso",
+      user: { name: user.name, email: user.email },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Erro interno do servidor" });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { error } = loginSchema.validate(req.body);
+    if (error)
+      return res.status(400).json({ message: error.details[0].message });
+
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email e senha são obrigatórios" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Email ou senha incorretos" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Email ou senha incorretos" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, name: user.name },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    res.status(200).json({ token, name: user.name });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Erro interno do servidor" });
+  }
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use("/users", userRoutes);
+
+mongoose.connect(process.env.MONGO_URI, {});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+export default app;
