fbfad1df658c1b4910dec6b536a941290e488c38
Arthur BARBOSA <arthurbarbo177@gmail.com>
2025-11-30 20:29:10 +0000

    introduzindo jwt e mantendo o usuario logado

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 app.use(express.json());
 
