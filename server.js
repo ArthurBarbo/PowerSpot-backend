e2118ba2d059b4f9c8c0f9d1c07ae1cb32fb65d0
Arthur BARBOSA <arthurbarbo177@gmail.com>
2025-12-26 19:15:50 +0000

    patch(ajustes): atualizações para deploy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "api.powerspot.rude.li",
+    origin: process.env.CORS_ORIGIN || "https://api.powerspot.rude.li",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
